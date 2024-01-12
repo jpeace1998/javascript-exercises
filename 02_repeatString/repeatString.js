a4d05ede1a533cd752601ee775771292b0c32c21
jpeace <jpeace1998@gmail.com>
2024-01-12 17:51:18 -0500

    Complete 7 Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inp_string, times) 
+{
+    if (times < 0) return "ERROR";
 
+    let string = "";
+
+    for (let i = 0; i < times; i++)
+    {
+        string += inp_string;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
